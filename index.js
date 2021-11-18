2c936de4669d3a9976bd701255323e4cf34765df
Gottfried Kleinberger <friedi.frieds@gmail.com>
2021-11-18 16:45:18 -0500

    logic for fetchCoordsByIP and fetchMyIP exist

---
+++ index.js
@@ -0,0 +1,25 @@
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
+
+// cb handles err or log only
+// fetchMyIP((err, body) => {
+//   if (err) {
+//     console.log(err);
+//     return;
+//   }
+//   console.log(body);
+// });
+
+// 45.72.235.116
+
+
+fetchCoordsByIP('45.72.235.116', (err, data) => {
+  if (err) {
+    console.log('this is error: ', err);
+    return;
+  }
+  console.log(data);
+});
+
+
+
+// throw is like return except the catch func will be looking for this at the end of a rrunctime.
