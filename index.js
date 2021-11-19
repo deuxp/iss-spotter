3323fdbb17dd4b2dc3376960d76326eb15befd35
Gottfried Kleinberger <friedi.frieds@gmail.com>
2021-11-18 19:03:24 -0500

    implement the logic for the callback, print date and times

--- index.js
+++ index.js
@@ -1,31 +1,18 @@
-const { fetchMyIP, fetchCoordsByIP, fetchFLyOverTimes } = require('./iss');
-
-// cb handles err or log only
-// fetchMyIP((err, body) => {
-//   if (err) {
-//     console.log(err);
-//     return;
-//   }
-//   console.log(body);
-// });
-
-// 45.72.235.116
+const { nextISSTimesForMyLocation } = require('./iss');
 
 
-// fetchCoordsByIP('45.72.235.116', (err, data) => {
-//   if (err) {
-//     console.log('this is error: ', err);
-//     return;
-//   }
-//   console.log(data);
-// });
-
-
-fetchFLyOverTimes({ latitude: 43.6859, longitude: -79.3974 }, (err, data) => {
-  if (err) {
-    console.log('this is error: ', err);
-    return;
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
   }
-  console.log(data);
-});
-// throw is like return except the catch func will be looking for this at the end of a rrunctime.
+  passTimes.response.forEach(flyby => {
+    console.log(`Next pass at ${new Date(flyby.risetime)} for ${flyby.duration} seconds!`);
+  });
+
+})
+
+
+
+
+
+
